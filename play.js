c1a7e5931410c810e9aebf86a69de0fd6fdd8c8f
Shrey Patel <shreypatel11@hotmail.com>
2022-06-08 15:33:05 -0400

    Make a new file for input function

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 // const net = require("net");
-const {connect} = require("./client")
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  return stdin;
-};
+setupInput();
